0dfb18d637f36f9ec57d33aa54783a48d1c04c61
Quentin Jungo <43724937+JungoQuentin@users.noreply.github.com>
2023-07-14 18:14:40 +0200

    Update primitives.rs

--- pg_sdl/src/primitives.rs
+++ pg_sdl/src/primitives.rs
@@ -107,7 +107,9 @@ pub fn draw_ellipse(
 			DrawRenderer::ellipse(canvas, position.x as i16, position.y as i16, radii.x as i16, radii.y as i16, color).unwrap();
 		};
 	} else {
-		todo!()
+		let rect =
+			Rect::new((position.x - radii.x) as i32, (position.y - radii.y) as i32, 2 * radii.x as u32, 2 * radii.y as u32);
+		DrawRenderer::ellipse(canvas, position.x as i16, position.y as i16, radii.x as i16, radii.y as i16, color).unwrap();
 	}
 }
 /// Draws a filled ellipse as seen by the camera
@@ -124,7 +126,10 @@ pub fn fill_ellipse(
 				.unwrap();
 		};
 	} else {
-		todo!()
+		let rect =
+			Rect::new((position.x - radii.x) as i32, (position.y - radii.y) as i32, 2 * radii.x as u32, 2 * radii.y as u32);
+		DrawRenderer::filled_ellipse(canvas, position.x as i16, position.y as i16, radii.x as i16, radii.y as i16, color)
+			.unwrap();
 	}
 }
 
@@ -138,7 +143,8 @@ pub fn draw_circle(canvas: &mut Canvas<Window>, camera: Option<&Camera>, color: 
 			DrawRenderer::circle(canvas, position.x as i16, position.y as i16, radius as i16, color).unwrap()
 		};
 	} else {
-		todo!()
+		let rect = Rect::new((position.x - radius) as i32, (position.y - radius) as i32, 2 * radius as u32, 2 * radius as u32);
+		DrawRenderer::circle(canvas, position.x as i16, position.y as i16, radius as i16, color).unwrap()
 	}
 }
 /// Draws a filled circle as seen by the camera
@@ -151,7 +157,8 @@ pub fn fill_circle(canvas: &mut Canvas<Window>, camera: Option<&Camera>, color: 
 			DrawRenderer::filled_circle(canvas, position.x as i16, position.y as i16, radius as i16, color).unwrap()
 		};
 	} else {
-		todo!()
+		let rect = Rect::new((position.x - radius) as i32, (position.y - radius) as i32, 2 * radius as u32, 2 * radius as u32);
+		DrawRenderer::filled_circle(canvas, position.x as i16, position.y as i16, radius as i16, color).unwrap()
 	}
 }
 
@@ -159,36 +166,40 @@ pub fn fill_circle(canvas: &mut Canvas<Window>, camera: Option<&Camera>, color: 
 pub fn draw_polygon(canvas: &mut Canvas<Window>, camera: Option<&Camera>, color: Color, vertices: &Vec<Point2<f64>>) {
 	if let Some(camera) = camera {
 		let vertices: Vec<Point2<f64>> = vertices.iter().map(|point| camera.transform * point).collect();
-		let vx: Vec<i16> = vertices.iter().map(|point| point.x as i16).collect();
-		let vy: Vec<i16> = vertices.iter().map(|point| point.y as i16).collect();
-		let x_min = *vx.iter().min().unwrap() as i32;
-		let y_min = *vy.iter().min().unwrap() as i32;
-		let x_max = *vx.iter().max().unwrap() as i32;
-		let y_max = *vy.iter().max().unwrap() as i32;
-		let rect = Rect::new(x_min, y_min, (x_max - x_min) as u32, (y_max - y_min) as u32);
+	}
+	let vx: Vec<i16> = vertices.iter().map(|point| point.x as i16).collect();
+	let vy: Vec<i16> = vertices.iter().map(|point| point.y as i16).collect();
+	let x_min = *vx.iter().min().unwrap() as i32;
+	let y_min = *vy.iter().min().unwrap() as i32;
+	let x_max = *vx.iter().max().unwrap() as i32;
+	let y_max = *vy.iter().max().unwrap() as i32;
+	let rect = Rect::new(x_min, y_min, (x_max - x_min) as u32, (y_max - y_min) as u32);
+	if let Some(camera) = camera {
 		if camera.is_in_scope(rect) {
-			DrawRenderer::polygon(canvas, &vx, &vy, color).unwrap();
+			DrawRenderer::filled_polygon(canvas, &vx, &vy, color).unwrap();
 		}
 	} else {
-		todo!()
+		DrawRenderer::filled_polygon(canvas, &vx, &vy, color).unwrap();
 	}
 }
 /// Draws a filled polygon from its vertices as seen by the camera
 pub fn fill_polygon(canvas: &mut Canvas<Window>, camera: Option<&Camera>, color: Color, vertices: &Vec<Point2<f64>>) {
 	if let Some(camera) = camera {
 		let vertices: Vec<Point2<f64>> = vertices.iter().map(|point| camera.transform * point).collect();
-		let vx: Vec<i16> = vertices.iter().map(|point| point.x as i16).collect();
-		let vy: Vec<i16> = vertices.iter().map(|point| point.y as i16).collect();
-		let x_min = *vx.iter().min().unwrap() as i32;
-		let y_min = *vy.iter().min().unwrap() as i32;
-		let x_max = *vx.iter().max().unwrap() as i32;
-		let y_max = *vy.iter().max().unwrap() as i32;
-		let rect = Rect::new(x_min, y_min, (x_max - x_min) as u32, (y_max - y_min) as u32);
+	}
+	let vx: Vec<i16> = vertices.iter().map(|point| point.x as i16).collect();
+	let vy: Vec<i16> = vertices.iter().map(|point| point.y as i16).collect();
+	let x_min = *vx.iter().min().unwrap() as i32;
+	let y_min = *vy.iter().min().unwrap() as i32;
+	let x_max = *vx.iter().max().unwrap() as i32;
+	let y_max = *vy.iter().max().unwrap() as i32;
+	let rect = Rect::new(x_min, y_min, (x_max - x_min) as u32, (y_max - y_min) as u32);
+	if let Some(camera) = camera {
 		if camera.is_in_scope(rect) {
 			DrawRenderer::filled_polygon(canvas, &vx, &vy, color).unwrap();
 		}
 	} else {
-		todo!()
+		DrawRenderer::filled_polygon(canvas, &vx, &vy, color).unwrap();
 	}
 }
 
@@ -199,37 +210,38 @@ pub fn draw_arrow(
 	if start == end {
 		return;
 	}
+	let mut start = start;
+	let mut end = end;
+	let mut width = width;
 	if let Some(camera) = camera {
-		let start = camera.transform * start;
-		let end = camera.transform * end;
-		let width = camera.scale() * width;
-		// TODO clean up
-		let x_dir = end - start;
-		let y_dir = x_dir.perpendicular() * width / 2.0;
-		let transform =
-			Transform2::from_matrix_unchecked(Matrix3::new(x_dir.x, y_dir.x, start.x, x_dir.y, y_dir.y, start.y, 0.0, 0.0, 1.0));
-
-		let head_back: f64 = 1.0 - 3.0 * width / x_dir.norm();
-
-		let mut points = Vec::from([
-			Point2::new(head_back, -1.0),
-			Point2::new(head_back, -3.0),
-			Point2::new(1.0, 0.0),
-			Point2::new(head_back, 3.0),
-			Point2::new(head_back, 1.0),
-		]);
-		if x_dir.norm() > 3.0 * width {
-			points.append(&mut Vec::from([Point2::new(0.0, 1.0), Point2::new(0.0, -1.0)]));
-		}
-		points.iter_mut().for_each(|v| *v = transform * *v);
-		let points_x: Vec<i16> = points.iter().map(|v| v.x as i16).collect();
-		let points_y: Vec<i16> = points.iter().map(|v| v.y as i16).collect();
-
-		DrawRenderer::filled_polygon(canvas, &points_x, &points_y, color).unwrap();
-		DrawRenderer::polygon(canvas, &points_x, &points_y, Colors::BLACK).unwrap();
-	} else {
-		todo!()
+		start = camera.transform * start;
+		end = camera.transform * end;
+		width = camera.scale() * width;
 	}
+	// TODO clean up
+	let x_dir = end - start;
+	let y_dir = x_dir.perpendicular() * width / 2.0;
+	let transform =
+		Transform2::from_matrix_unchecked(Matrix3::new(x_dir.x, y_dir.x, start.x, x_dir.y, y_dir.y, start.y, 0.0, 0.0, 1.0));
+
+	let head_back: f64 = 1.0 - 3.0 * width / x_dir.norm();
+
+	let mut points = Vec::from([
+		Point2::new(head_back, -1.0),
+		Point2::new(head_back, -3.0),
+		Point2::new(1.0, 0.0),
+		Point2::new(head_back, 3.0),
+		Point2::new(head_back, 1.0),
+	]);
+	if x_dir.norm() > 3.0 * width {
+		points.append(&mut Vec::from([Point2::new(0.0, 1.0), Point2::new(0.0, -1.0)]));
+	}
+	points.iter_mut().for_each(|v| *v = transform * *v);
+	let points_x: Vec<i16> = points.iter().map(|v| v.x as i16).collect();
+	let points_y: Vec<i16> = points.iter().map(|v| v.y as i16).collect();
+
+	DrawRenderer::filled_polygon(canvas, &points_x, &points_y, color).unwrap();
+	DrawRenderer::polygon(canvas, &points_x, &points_y, Colors::BLACK).unwrap();
 }
 
 /// Draws text as seen by the camera
@@ -250,6 +262,10 @@ pub fn draw_text(
 			text_drawer.draw(canvas, position, text_style, &text, align);
 		}
 	} else {
-		todo!()
+		let position = nalgebra::Point2::new(position.x as f64, position.y as f64);
+		let text_style = &TextStyle { font_size: font_size as u16, color, ..Default::default() };
+		let (width, height) = text_drawer.text_size(text_style, &text);
+		let rect = Rect::new(position.x as i32, position.y as i32, width, height);
+		text_drawer.draw(canvas, position, text_style, &text, align);
 	}
 }
